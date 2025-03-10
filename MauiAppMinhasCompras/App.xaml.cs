47f332f86b5df3259bf3c92b577f1df6ea6d36fd
GIOVANA MIURA <giovana.miura@etec.sp.gov.br>
2025-03-10 08:40:28 -0300

    add de insert

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,9 +1,31 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
-        public App()
+        static SQLiteDatabaseHelper _db;  //_db = campo - onde o dado está gravado
+
+        public static SQLiteDatabaseHelper Db    //Db = propriedade - forma de acesso ao campo
         {
+            get
+            {
+                if (_db == null)  //se campo _db for nulo
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.bd3");  //nome do arquivo
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+
+        }
+
+        public App()
+        { 
             InitializeComponent();
 
             // MainPage = new AppShell();
